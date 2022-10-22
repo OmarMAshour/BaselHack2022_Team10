78c94f5cbce16e34630370b9e22bbbd4eebce067
OmarMAshour <omar_m_ashour@yahoo.com>
2022-10-22 19:53:25 +0200

    added db and admin configured

--- coop_challenge/models.py
+++ coop_challenge/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Scanned_Wine(models.Model):
-    
+class ScannedWine(models.Model):
+    name = models.TextField()
+    article_id = models.TextField()
+    alcohol = models.TextField()
+    sellPrice = models.TextField()
+    averageRating = models.TextField()
+    maturity = models.TextField()
+    goesWithText = models.TextField()
+    goesWithText_de = models.TextField()
+    goesWithText_en = models.TextField()
+    goesWithText_fr = models.TextField()
+    goesWithText_it = models.TextField()
+    servingTemperature = models.TextField()
+    wineCharacter = models.TextField()
+    wineMaker = models.TextField()
+    wineOrigin = models.TextField()
+    yearOfVintage = models.TextField()
     json = models.TextField()
+
+    def __str__(self) -> str:
+        return self.name

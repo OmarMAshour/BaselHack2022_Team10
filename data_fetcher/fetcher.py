17a57d05aedb88a45647f87ca733e9ef0d1ffff8
huangrpaulo <huangrui.pablo@icloud.com>
2022-10-22 15:18:37 +0200

    data fetcher first commit, api.conf not included

---
+++ data_fetcher/fetcher.py
@@ -0,0 +1,75 @@
+"""
+Fetcher module, this is where all the data fetch from Wine API happens
+"""
+
+import requests
+import configparser
+from typing import List, Dict
+import ast
+
+label_keys = [
+    "alcohol",
+    "allPrices",
+    "baseMaterialNumber",
+    "code",
+    "enjoyFrom",
+    "enjoyUntil",
+    "grapesText",
+    "images",
+    "name",
+    "wineAgeing",
+    "wineCharacter",
+    "wineMaker",
+    "wineOrigin",
+    "wineOriginCountry",
+    "wineOriginRegion",
+    "yearOfVintage"
+]
+
+
+class Fetcher:
+
+    def __init__(self) -> None:
+        # read api configuration (secret)
+        self.api_conf = configparser.RawConfigParser()
+        self.api_conf.read("api.conf")
+        api_details = dict(self.api_conf.items("Wine Info"))
+
+        self.url = api_details['url']
+        self.api_key = api_details['api_key']
+
+        # read keys configuration for label and info sheet
+        self.keys_conf = configparser.RawConfigParser()
+        self.keys_conf.read("keys.conf")
+        self.label_keys = ast.literal_eval(self.keys_conf.get("Keys", "label"))
+
+    def find_for_labels(self, article_ids: List[str]) -> List[Dict]:
+        return self.find_by_article_ids(article_ids, self.label_keys)
+
+    def find_by_article_ids(self, article_ids: List[str], keys: List[str] = None) -> List[Dict]:
+        """
+        Fetch a list of wine info by a list of article_ids
+        """
+
+        params = {"codes": article_ids}
+        headers = {"APIKey": self.api_key}
+
+        response = requests.get(
+            self.url,
+            params=params,
+            headers=headers
+        )
+
+        products = response.json()["products"]
+
+        if keys is None:
+            return products
+
+        return [{key: product[key] for key in keys} for product in products]
+
+
+if __name__ == "__main__":
+    fetcher = Fetcher()
+    items = fetcher.find_for_labels(article_ids=["1007906010"])
+    print(items)
+

3b5127d6b835cc314c1b4e8ead41f92986b2178d
OmarMAshour <omar_m_ashour@yahoo.com>
2022-10-23 10:57:02 +0200

    done with articles routing

--- coop_challenge/views.py
+++ coop_challenge/views.py
@@ -9,3 +9,28 @@ def viewIndex(request):
     # return FileResponse(open(tryPDF(), 'rb'), content_type='application/pdf')
 
     return render(request, 'coop_challenge/scanner_1.html', {})
+
+def printLabel(request, article):
+    # tryPDF()
+    # return FileResponse(open(tryPDF(), 'rb'), content_type='application/pdf')
+    print(article)
+    print(article)
+    print(article)
+    print(article)
+    print(article)
+    print(article)
+    print(article)
+    print(article)
+    return render(request, 'coop_challenge/scanner_1.html', {})
+
+def printLabell(request):
+    # tryPDF()
+    # return FileResponse(open(tryPDF(), 'rb'), content_type='application/pdf')
+
+    return render(request, 'coop_challenge/scanner_1.html', {})
+
+def printPdf(request, article):
+    # tryPDF()
+    # return FileResponse(open(tryPDF(), 'rb'), content_type='application/pdf')
+
+    return render(request, 'coop_challenge/scanner_1.html', {})
